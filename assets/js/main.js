f24d8aeed88482811c841f788000f81af6e25687
SerjioBlack <serjioblack@gmail.com>
2023-09-12 06:06:56 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+window.addEventListener('scroll', function() {
+    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
+});
+
+$(document).ready(function(){
+    $(".owl-carousel-full").owlCarousel({
+        loop:true,
+        autoplay:true,
+    margin:20,
+    dots:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        700:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+    });
+});
+
+/*const offcanvasCartEl = document.getElementById('offcanvasCart');
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+document.querySelectorAll('.closecart').forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        offcanvasCart.hide();
+        // let href = item.href.split('#').pop();
+        const href = item.dataset.href;
+        offcanvasCartEl.addEventListener('hidden.bs.offcanvas', () => {
+            document.getElementById(href).scrollIntoView();
+        });
+    });
+});*/
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+});
+
+    $('#top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+})
